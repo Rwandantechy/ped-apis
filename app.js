1b09875707903c6f25e74e7b7df865e1f5e210ca
Rwandantechy <niyinnocent2027@gmail.com>
2023-09-22 18:59:03 +0530

    have been working on the modifications ofswagger UI and changing configuration of doumentation from routes folder to its own folder for easy maintainance.

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ import morgan from "morgan";
 import swaggerUI from "swagger-ui-express";
 import blogsRoutes from "./Routes/blogs.js ";
 import swaggerOptions from "./Documentation/swagger.js";
-
-
 const app = express();
 app.use(morgan("dev"));
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
