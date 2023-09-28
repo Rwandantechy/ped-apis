72ef567574690202ca5762413fb9ec75a0f8ac18
Rwandantechy <niyinnocent2027@gmail.com>
2023-09-28 15:37:26 +0530

    have made all basic CRUD operations working fine create content , get all created blogs, get single blog , update a created blog, and delete any created blog

--- Controllers/blogsController.js
+++ Controllers/blogsController.js
@@ -1,22 +1,27 @@
 import { getCollection } from "../database.js";
 import { ObjectId } from "mongodb";
-// Create a new blog
+import fs from "fs";
+import path from "path";
+
+// Create a new blog or any content type.
 export const createBlog = async (req, res) => {
   try {
     const blogsCollection = getCollection("blogs");
     const blogData = req.body;
     const file = req.file;
     if (file) {
+      console.log("Uploaded file:", file);
       blogData.image = file.path;
+
     }
 
     const result = await blogsCollection.insertOne(blogData);
 
     const createdBlog = {
       _id: result.insertedId,
-      title: blogData.title,
-      content: blogData.content,
-      image: blogData.image,
+      title: result.title,
+      content: result.content,
+      image: result.image,
     };
 
     res.status(201).json({
@@ -24,35 +29,28 @@ export const createBlog = async (req, res) => {
       blog: createdBlog,
       request: {
         type: "GET",
-        description: "Get the created blogpost",
+        description: "Get the created blogpost/any type",
         url: `/api/v1/ped/blogs/${result.insertedId}`,
       },
     });
   } catch (error) {
-    console.error("Error adding a blogpost:", error);
+    console.error("Error creating the content:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-// Get all blogs
+// Get all blogs/any content type.
 export const getAllBlogs = async (req, res) => {
   try {
     const blogsCollection = getCollection("blogs");
     const blogs = await blogsCollection.find({}).toArray();
-
-    const formattedBlogs = blogs.map((blog) => ({
-      _id: blog._id,
-      title: blog.title,
-      content: blog.content,
-      hasImage: blog.image,
-    }));
 
     res.status(200).json({
       metadata: {
         count: blogs.length,
         message: "List of all created blogs retrieved successfully.",
       },
-      blogs: formattedBlogs,
+      blogs: blogs,
       request: {
         type: "GET",
         description: "Get all blogs",
@@ -69,78 +67,82 @@ export const getAllBlogs = async (req, res) => {
 export const getBlogById = async (req, res) => {
   try {
     const id = `${req.params.id}`;
-    console.log(id);
     if (!id) {
-      return res.status(400).json({ error: "_id parameter is required" });
+      return res
+        .status(400)
+        .json({ error: "be specific about the blog you want to retrieve" });
     }
     const blogsCollection = getCollection("blogs");
     const blog = await blogsCollection.findOne(new ObjectId(id));
-    console.log(id);
     if (!blog) {
-      console.log(id);
-      return res.status(404).json({ error: "Blog not found" });
+      return res
+        .status(404)
+        .json({ error: "The blog that you are looking is not available" });
     }
     const formattedBlog = {
       _id: blog._id,
       title: blog.title,
       content: blog.content,
       image: blog.image,
+      ...blog,
     };
 
     res.status(200).json({
       blog: formattedBlog,
       request: {
         type: "GET",
-        description: "Get the blog",
-        url: `/api/v1/ped/blogs/${id}`,
+        description: "You can still see all blogs",
+        url: `/api/v1/ped/blogs`,
       },
     });
   } catch (error) {
-    console.error("Error fetching a blog:", error);
+    console.error("Error fetching a blogpost:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
-
 // Update an existing blog
 export const updateBlog = async (req, res) => {
   const id = `${req.params.id}`;
-  const blogData = req.body;
-  const file = req.file;
-
-  if (file) {
-    blogData.image = file.path;
-  }
-
+  const blogUpdatedData = {};
+  const file = req.file; 
   try {
     const blogsCollection = getCollection("blogs");
 
-    // Check if id is a valid ObjectId
     if (!ObjectId.isValid(id)) {
-      return res.status(400).json({ error: "Invalid ObjectId" });
+      return res
+        .status(400)
+        .json({ error: "Invalid ObjectId. You can't update content then." });
     }
 
-    // Ensure blogData is an object
-    if (typeof blogData !== "object" || blogData === null) {
+    if (typeof blogUpdatedData !== "object" || blogUpdatedData === null) {
       return res.status(400).json({ error: "Invalid update data" });
     }
 
-    const updateQuery = { $set: {} };
+    if (file) {      
+      const uploadDir = "./blogImages/";
+      const originalName = file.originalname;
+      const timestamp = Date.now();
+      const filename = `${timestamp}_${originalName}`;
 
-    for (const key in blogData) {
-      // Only update fields that exist in the blogData object
-      if (blogData.hasOwnProperty(key)) {
-        updateQuery.$set[key] = blogData[key];
-      }
+       const filePath = uploadDir + filename;      
+      fs.renameSync(file.path, filePath);
+      blogUpdatedData.image = filePath;
+    }
+
+    for (const key in req.body) {
+      blogUpdatedData[key] = req.body[key];
     }
 
     const updatedBlog = await blogsCollection.findOneAndUpdate(
       { _id: new ObjectId(id) },
-      updateQuery,
+      { $set: blogUpdatedData },
       { returnDocument: "after" }
     );
 
     if (updatedBlog === null) {
-      return res.status(404).json({ error: "Blog not found" });
+      return res
+        .status(404)
+        .json({ error: "Blog not found, content didn't get updated." });
     }
 
     const formattedBlog = {
@@ -153,7 +155,7 @@ export const updateBlog = async (req, res) => {
       blog: formattedBlog,
       request: {
         type: "GET",
-        description: "Get the updated blog",
+        description: "Get the content of updated blog",
         url: `/api/v1/ped/blogs/${id}`,
       },
     });
@@ -165,14 +167,19 @@ export const updateBlog = async (req, res) => {
 
 // Delete a blog by ID
 export const deleteBlog = async (req, res) => {
-  const { id } = req.params;
+  const id = `${req.params.id}`;
 
   try {
     const blogsCollection = getCollection("blogs");
-    const result = await blogsCollection.deleteOne({ _id: id });
+    console.log(id);
+    const result = await req.blogsCollection.deleteOne({
+      _id: new ObjectId(id),
+    });
 
     if (result.deletedCount === 0) {
-      return res.status(404).json({ error: "Blog not found" });
+      return res
+        .status(404)
+        .json({ error: "Blog not found,can't perform delete" });
     }
 
     res.status(200).json({
